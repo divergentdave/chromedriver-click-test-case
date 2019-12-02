9f8dc8f120be21a63e6614330947826e38d687ca
David Cook <divergentdave@gmail.com>
2019-12-01 19:52:48 -0600

    Script to run repeatedly with different containers

---
+++ harness.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python3
+import time
+import subprocess
+
+from ruamel.yaml import YAML
+
+
+def run_trials():
+    test_args = ["docker", "exec", "-it", "test-case-web", "python", "test.py"]
+    success_count = 0
+    failure_count = 0
+    for i in range(100):
+        result = subprocess.run(test_args)
+        if result.returncode == 0:
+            success_count += 1
+        else:
+            failure_count += 1
+    return success_count, failure_count
+
+
+def run_with_compose(compose_file="docker-compose.yml"):
+    subprocess.run(["docker-compose", "-f", compose_file, "build"], check=True)
+    docker = subprocess.Popen(
+        ["docker-compose", "-f", compose_file, "up"],
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.DEVNULL,
+    )
+    time.sleep(30)
+    success_count, failure_count = run_trials()
+    docker.terminate()
+    docker.wait()
+    print("{} successes, {} failures".format(success_count, failure_count))
+
+
+def main():
+    yaml = YAML(typ="rt")
+    with open("docker-compose.yml", "rb") as f:
+        original_config = yaml.load(f)
+    new_compose_file_name = "docker-compose.modified.yml"
+    versions = [
+        "3.141.59-xenon",
+        "3.141.59-vanadium",
+        "3.141.59-uranium",
+        "3.141.59-titanium",
+        "3.141.59-selenium",
+        "3.141.59-radium",
+        "3.141.59-palladium",
+        "3.141.59-oxygen",
+        "3.141.59-neon",
+        "3.141.59-mercury",
+        "3.141.59-lithium",
+        "3.141.59-krypton",
+        "3.141.59-iron",
+        "3.141.59-hafnium",
+        "3.141.59-gold",
+        "3.141.59-fluorine",
+        "3.141.59-europium",
+        "3.141.59-dubnium",
+        "3.141.59-copernicium",
+        "3.141.59-bismuth",
+        "3.141.59-antimony",
+    ]
+    for version in versions:
+        image = "selenium/standalone-chrome-debug:{}".format(version)
+        modified_config = original_config.copy()
+        modified_config["services"]["selenium"]["image"] = image
+        with open(new_compose_file_name, "wb") as f:
+            yaml.dump(modified_config, f)
+        print(image)
+        run_with_compose(new_compose_file_name)
+        print(image)
+
+
+if __name__ == "__main__":
+    main()
--- web/test.py
+++ web/test.py
@@ -6,7 +6,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.ui import WebDriverWait
 
-SELENIUM_TIMEOUT = 30
+SELENIUM_TIMEOUT = 10
 
 
 def main():
